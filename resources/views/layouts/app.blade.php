8c821ecdd7f2b841f9bb878f120b4e7f7f8f7829
Dheeraj <dheerajkapuganti123@gmail.com>
2026-09-08 10:25:08 +0530

    Fix HTTPS asset loading

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Startup Platform Auth</title>
     <!-- Link to the custom CSS we built -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
 </head>
 <body>
     @yield('content')
